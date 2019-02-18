d23852d447826683386fc76f1631b2e54aaf1cf0
archthegit <archieprad@gmail.com>
2019-02-18 11:12:37 +0800

    Random forest with HAR data

---
+++ RF/randomforest.py
@@ -0,0 +1,33 @@
+from pandas import read_csv
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+
+def load_file(filepath):
+	data_frame = read_csv(filepath, header=None, delim_whitespace =True)
+	return data_frame.values
+
+def load_dataset_group(group,prefix = ''):
+	X = load_file(prefix+group+'/X_'+group+'.txt')
+	y = load_file(prefix+group+'/y_'+group+'.txt')
+	return X,y
+
+def load_dataset(prefix=''):
+	trainX, trainy = load_dataset_group('train',prefix+'./HARDataset/')
+	print(trainX.shape, trainy.shape)
+
+	testX,testy = load_dataset_group('test','./HARDataset/')
+	print(testX.shape, testy.shape)
+
+	trainy, testy = trainy[:,0], testy[:,0]
+	return trainX, trainy, testX, testy
+
+def evaluate_model (trainX,trainy, testX,testy,model):
+	model.fit(trainX,trainy);
+	pred = model.predict(testX);
+	accuracy = accuracy_score(pred,testy);
+	return accuracy*100.0
+
+trainX,trainy, testX, testy = load_dataset()
+model = RandomForestClassifier(n_estimators =98)
+results = evaluate_model(trainX,trainy,testX,testy,model)
+print('results = %.3f' %results);
